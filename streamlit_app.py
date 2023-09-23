2f90268b26cf0aedc46c603304f9f11a07a7ce20
Wilson <70288156+inunoriginal1@users.noreply.github.com>
2023-09-22 18:32:11 -0700

    Update streamlit_app.py
    Updated streamlit import to use standard st abbreviation.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,15 @@
-import streamlit
+import streamlit as st
 
-streamlit.title("My Parent's New Healthy Diner")
+st.title("My Parent's New Healthy Diner")
 
-streamlit.header("Breakfast Favourites")
-streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔 Hard-Boiled, Free-Range Egg")
-streamlit.text("🥑🍞 Avocado Toast")
+st.header("Breakfast Favourites")
+st.text("🥣 Omega 3 & Blueberry Oatmeal")
+st.text("🥗 Kale, Spinach & Rocket Smoothie")
+st.text("🐔 Hard-Boiled, Free-Range Egg")
+st.text("🥑🍞 Avocado Toast")
 
-streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+st.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 
 import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+st.dataframe(my_fruit_list)
